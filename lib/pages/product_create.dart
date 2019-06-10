807c713aeaf9753bb5905bcece8daa04c3cded90
ShankarDhandapani <shankardhandapani1@gmail.com>
2019-06-10 19:32:50 +0530

    Added some style to form

--- lib/pages/product_create.dart
+++ lib/pages/product_create.dart
@@ -16,33 +16,37 @@ class _ProductCreatePageSate extends State<ProductCreatePage> {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: <Widget>[
-        TextField(
-          decoration: InputDecoration(labelText: 'Product Title'),
-          onChanged: (String value) {
-            setState(() {
-              titleValue = value;
-            });
-          },
-        ),
-        TextField(
-          
-          maxLines: 4,
-          onChanged: (String value) {
-            setState(() {
-              descriptionValue = value;
-            });
-          },
-        ),TextField(
-          keyboardType: TextInputType.number,
-          onChanged: (String value) {
-            setState(() {
-              priceValue = double.parse(value);
-            });
-          },
-        ),
-      ],
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(labelText: 'Product Title', ),
+            onChanged: (String value) {
+              setState(() {
+                titleValue = value;
+              });
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(labelText: 'Product description'),
+            maxLines: 4,
+            onChanged: (String value) {
+              setState(() {
+                descriptionValue = value;
+              });
+            },
+          ),TextField(
+            decoration: InputDecoration(labelText: 'Product Prize'),
+            keyboardType: TextInputType.number,
+            onChanged: (String value) {
+              setState(() {
+                priceValue = double.parse(value);
+              });
+            },
+          ),
+        ],
+      ),
     );
   }
 }
