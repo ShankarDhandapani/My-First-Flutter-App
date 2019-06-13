7ba2b163627293a187a39b3e6eeec83f9f130be1
ShankarDhandapani <shankardhandapani1@gmail.com>
2019-06-13 11:32:18 +0530

    Added the background image to the auth page

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -20,7 +20,11 @@ class _AuthPageState extends State<AuthPage> {
         title: Text('Login'),
       ),
       body: Container(
-        margin: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+                 image: AssetImage('assets/background.jpg'))),
         child: ListView(
           children: <Widget>[
             TextField(
@@ -43,7 +47,7 @@ class _AuthPageState extends State<AuthPage> {
               value: _acceptTerms,
               onChanged: (bool value) {
                 setState(() {
-                 _acceptTerms = value; 
+                  _acceptTerms = value;
                 });
               },
               title: Text('Accept Terms'),
